7e8f0ab3dc0745ac27d2fb670741c2735178b3e7
MiverMor <ivan.mordyashev@mail.ru>
2026-07-01 16:52:07 +0800

    implement sticky header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { initScrollSpy } from './modules/scroll-spy.js';
+import { initStickyHeader } from './modules/sticky-header.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initScrollSpy();
+  initStickyHeader();
 });
